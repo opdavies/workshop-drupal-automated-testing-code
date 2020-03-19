dcace271a237002d7b1988cee2dbd01ce986855e
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 21:14:48 +0000

    4f: Build up the getAll() method
    Inject the entity type manager service into the ArticleRepository, and
    use it to load and return all nodes.
    
    This fails as not enough arguments are passed to the article repository
    service from the container.

--- web/modules/custom/my_module/src/Repository/ArticleRepository.php
+++ web/modules/custom/my_module/src/Repository/ArticleRepository.php
@@ -2,10 +2,21 @@
 
 namespace Drupal\my_module\Repository;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+
 class ArticleRepository {
 
+  /**
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  private $nodeStorage;
+
+  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+    $this->nodeStorage = $entityTypeManager->getStorage('node');
+  }
+
   public function getAll(): array {
-    return [];
+    return $this->nodeStorage->loadMultiple();
   }
 
 }
