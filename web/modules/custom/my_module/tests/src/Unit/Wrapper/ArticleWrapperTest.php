76172f4667ec243ee07847b39cf4446238c63968
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 21:55:28 +0000

    5b: Add a test for which nodes are wrappable
    If we attempt to wrap a node which is not an article, we expect an
    Exception to be thrown. As one currently isn’t, the test will fail.

--- web/modules/custom/my_module/tests/src/Unit/Wrapper/ArticleWrapperTest.php
+++ web/modules/custom/my_module/tests/src/Unit/Wrapper/ArticleWrapperTest.php
@@ -21,5 +21,15 @@ class ArticleWrapperTest extends UnitTestCase {
     $this->assertSame('article', $articleWrapper->getOriginal()->bundle());
   }
 
+  /** @test */
+  public function it_throws_an_exception_if_the_node_is_not_an_article() {
+    $this->expectException(\InvalidArgumentException::class);
+
+    $page = $this->createMock(NodeInterface::class);
+    $page->method('bundle')->willReturn('page');
+
+    new ArticleWrapper($page);
+  }
+
 }
 
