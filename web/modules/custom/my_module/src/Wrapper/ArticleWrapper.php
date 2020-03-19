3ecca5ad0c42a489108db32bdbb757fbf156b600
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 21:58:07 +0000

    5b: Throw an Exception is a non-article is wrapped
    Within the article wrapper, let’s check that the node is an article,
    otherwise thrown the Exception as expected.

--- web/modules/custom/my_module/src/Wrapper/ArticleWrapper.php
+++ web/modules/custom/my_module/src/Wrapper/ArticleWrapper.php
@@ -9,11 +9,22 @@ class ArticleWrapper {
   private $article;
 
   public function __construct(NodeInterface $node) {
+    $this->verifyNodeType($node);
+
     $this->article = $node;
   }
 
   public function getOriginal(): NodeInterface {
     return $this->article;
+  }
+
+  private function verifyNodeType(NodeInterface $node): void {
+   if ($node->bundle() != 'article') {
+     throw new \InvalidArgumentException(sprintf(
+         '%s is not an article',
+         $node->bundle()
+       ));
+    }
   }
 
 }
