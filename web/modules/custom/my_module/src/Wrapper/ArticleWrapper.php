b98d181a58a468ce7a54bcc4ba838625f9bb28c1
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 22:01:10 +0000

    5c: Add isPublishable method to article wrapper
    Add the `isPublishable()` method to the article wrapper. This contains
    all of the logic that is used to determine if an article in publishable
    or not, and returns a boolean.

--- web/modules/custom/my_module/src/Wrapper/ArticleWrapper.php
+++ web/modules/custom/my_module/src/Wrapper/ArticleWrapper.php
@@ -2,20 +2,30 @@
 
 namespace Drupal\my_module\Wrapper;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\node\NodeInterface;
 
 class ArticleWrapper {
 
   private $article;
+  private $time;
 
-  public function __construct(NodeInterface $node) {
+  public function __construct(TimeInterface $time, NodeInterface $node) {
     $this->verifyNodeType($node);
 
+    $this->time = $time;
     $this->article = $node;
   }
 
   public function getOriginal(): NodeInterface {
     return $this->article;
+  }
+
+  public function isPublishable(): bool {
+    $created = $this->article->getCreatedTime();
+    $difference = $this->time->getRequestTime() - $created;
+
+    return $difference >= 60 * 60 * 24 * 3;
   }
 
   private function verifyNodeType(NodeInterface $node): void {
