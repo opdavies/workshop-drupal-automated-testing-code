fd921d3e4cb099306f91219b5011c9c425dac31e
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 20:33:41 +0000

    1d: Add first functional test
    Ensure that anonymous users can view the site’s front page.

---
+++ web/modules/custom/my_module/tests/src/Functional/MyModuleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\my_module\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+use Symfony\Component\HttpFoundation\Response;
+
+class MyModuleTest extends BrowserTestBase {
+
+  protected $defaultTheme = 'stark';
+
+  protected static $modules = ['my_module'];
+
+   /** @test */
+  public function the_front_page_loads_for_anonymous_users() {
+    $this->drupalGet('<front>');
+
+    $this->assertResponse(Response::HTTP_OK);
+  }
+
+}
