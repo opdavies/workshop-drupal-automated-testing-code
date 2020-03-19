9484d924a16e49b3e98b24cbe648bd2925dfded6
Oliver Davies <oliver@oliverdavies.uk>
2020-03-19 21:00:51 +0000

    3e: Add page text
    Replace the empty render array with one that returns the expected text.

--- web/modules/custom/my_module/src/Controller/BlogPageController.php
+++ web/modules/custom/my_module/src/Controller/BlogPageController.php
@@ -2,10 +2,16 @@
 
 namespace Drupal\my_module\Controller;
 
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
 class BlogPageController {
 
+  use StringTranslationTrait;
+
   public function __invoke(): array {
-    return [];
+    return [
+      '#markup' => $this->t('Welcome to my blog!'),
+    ];
   }
 
 }
