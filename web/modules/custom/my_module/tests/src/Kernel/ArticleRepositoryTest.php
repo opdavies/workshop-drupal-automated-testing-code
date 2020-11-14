48c27247362a2bfa5fa2959d5b879e4c6d309cf9
Oliver Davies <oliver@oliverdavies.uk>
2020-11-14 09:12:58 +0000

    4g: Add an article

--- web/modules/custom/my_module/tests/src/Kernel/ArticleRepositoryTest.php
+++ web/modules/custom/my_module/tests/src/Kernel/ArticleRepositoryTest.php
@@ -4,13 +4,18 @@ namespace Drupal\Tests\my_module\Kernel;
 
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
 use Drupal\my_module\Repository\ArticleRepository;
+use Drupal\Tests\node\Traits\NodeCreationTrait;
 
 class ArticleRepositoryTest extends EntityKernelTestBase {
+
+  use NodeCreationTrait;
 
   public static $modules = ['node', 'my_module'];
 
   /** @test */
   public function it_returns_blog_posts() {
+    $this->createNode(['type' => 'article', 'title' => 'Test post'])->save();
+
     $repository = $this->container->get(ArticleRepository::class);
     $articles = $repository->getAll();
 
