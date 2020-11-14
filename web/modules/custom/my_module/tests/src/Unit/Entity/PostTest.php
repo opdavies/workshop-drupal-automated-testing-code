07ad4dc2b16e468e24d97073a49a16fc6b181ab0
Oliver Davies <oliver@oliverdavies.uk>
2020-11-14 09:57:54 +0000

    5b: Check that it returns the post title
    > Drupal\Tests\my_module\Unit\Entity\PostTest::it_returns_the_title
    > Error: Call to undefined method Drupal\my_module\Entity\Post::getTitle()

--- web/modules/custom/my_module/tests/src/Unit/Entity/PostTest.php
+++ web/modules/custom/my_module/tests/src/Unit/Entity/PostTest.php
@@ -2,8 +2,16 @@
 
 namespace Drupal\Tests\my_module\Unit\Entity;
 
+use Drupal\my_module\Entity\Post;
 use Drupal\Tests\UnitTestCase;
 
 class PostTest extends UnitTestCase {
+
+  /** @test */
+  public function it_returns_the_title() {
+    $post = new Post();
+
+    $this->assertSame('Test post', $post->getTitle());
+  }
 
 }
